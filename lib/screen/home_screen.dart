98a4e404eaca85116ed980068f4c86d98dff41dd
xMiCx <70638947+Mickuppom@users.noreply.github.com>
2021-03-21 10:00:08 +0700

    Initial commit

---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class HomeScreen extends StatefulWidget {
+  HomeScreen({Key key}) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  Future fatchTodo() async {
+    http.Response request = await http.get(
+      Uri.parse("https://jsonplaceholder.typicode.com/todos"),
+    );
+
+    print(request.body);
+  }
+
+  @override
+  void initState() {
+    fatchTodo();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home Screen"),
+      ),
+      body: Container(
+        child: Center(),
+      ),
+    );
+  }
+}
